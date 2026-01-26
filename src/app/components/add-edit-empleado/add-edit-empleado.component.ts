5eeb2d189b6452fd813583491b2f37b7a305d835
Wilfredo Abregu <fabregu01@gmail.com>
2026-01-25 22:06:49 -0500

    feat: Form add-edit

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule, MatOption } from '@angular/material/core';
+import { MatSelect } from "@angular/material/select";
 
 @Component({
   selector: 'app-add-edit-empleado',
   standalone: true,
-  imports: [],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatSelect,
+    MatOption
+],
   templateUrl: './add-edit-empleado.component.html',
   styleUrl: './add-edit-empleado.component.css'
 })
 export class AddEditEmpleadoComponent {
-
+  estadosCiviles: any[] = ['Soltero', 'Casado', 'Divorciado']
 }
