e07c16e522977fd70a3dc71e280a51b675943d0f
Wilfredo Abregu <fabregu01@gmail.com>
2026-01-26 22:07:16 -0500

    feat: Enlazar formulario

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -4,6 +4,10 @@ import { MatInputModule } from '@angular/material/input';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatNativeDateModule, MatOption } from '@angular/material/core';
 import { MatSelect } from "@angular/material/select";
+import {MatRadioModule} from '@angular/material/radio';
+import { JsonPipe } from '@angular/common';
+import { RouterLink } from "@angular/router";
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-edit-empleado',
@@ -14,11 +18,30 @@ import { MatSelect } from "@angular/material/select";
     MatDatepickerModule,
     MatNativeDateModule,
     MatSelect,
-    MatOption
+    MatOption,
+    MatRadioModule,
+    RouterLink,
+    ReactiveFormsModule,
+    JsonPipe
 ],
   templateUrl: './add-edit-empleado.component.html',
   styleUrl: './add-edit-empleado.component.css'
 })
 export class AddEditEmpleadoComponent {
-  estadosCiviles: any[] = ['Soltero', 'Casado', 'Divorciado']
+  estadosCiviles: any[] = ['Soltero', 'Casado', 'Divorciado'];
+
+  myForm: FormGroup;
+
+  constructor(private fb: FormBuilder){
+    this.myForm = this.fb.group({
+      nombres: [''] ,
+      correo: [''] ,
+      fechaIngreso: [''] ,
+      telefono: [''] ,
+      estadoCivil: [''] ,
+      sexo: [''] 
+    });
+  }
+
+
 }
